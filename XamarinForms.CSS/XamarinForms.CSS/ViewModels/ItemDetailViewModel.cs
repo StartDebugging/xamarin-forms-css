d11ef768ffb48cf395212a83f37f1161374b91cb
Marius Bughiu <marius.bughiu@gmail.com>
2018-04-18 18:05:41 +0300

    innitial commit

---
+++ XamarinForms.CSS/XamarinForms.CSS/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinForms.CSS.Models;
+
+namespace XamarinForms.CSS.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
